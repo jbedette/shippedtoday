7c8c43da958480e721b99b1cb6ff98b35e268a84
jbedette <jbedette@yahoo.com>
2016-11-02 12:04:15 -0400

    created shippedtoday db

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root '/home#index', as: 'homepage'
+  root 'home#index', as: 'homepage'
 end
