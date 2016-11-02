0a67cae4477b2a259e83ad7358166d976da8ca05
Arnold Hundert <hunderta@yahoo.com>
2016-11-02 14:00:55 -0400

    1st time adding to github master

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,34 @@
+class JobsController < ApplicationController
+
+  def index
+    @message = "This is the index page."
+    @jobs = Job.all
+    @ships = Ship.all
+  end
+
+  def new
+    @message = "This is the new page."
+  end
+
+  def create
+    # There is no view for the create action.
+  end
+
+  def show
+    @message = "This is the show page."
+  end
+
+  def edit
+    @message = "This is the edit page."
+  end
+
+  def update
+    # There is no view for the update action.
+  end
+
+  def destroy
+    # There is no view for the destroy  action.
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index', as: 'homepage'
+
+  # Jobs Routes
+  get     "/jobs"              =>     "jobs#index"
+  get     "/jobs/new"          =>     "jobs#new"
+  post    "/jobs"              =>     "jobs#create"
+  get     "/jobs/:id"          =>     "jobs#show"
+  get     "/jobs/:id/edit"     =>     "jobs#edit"
+  put     "/jobs/:id"          =>     "jobs#update"
+  patch   "/jobs/:id"          =>     "jobs#update"
+  delete  "/jobs/:id"          =>     "jobs#destroy"
+
 end
