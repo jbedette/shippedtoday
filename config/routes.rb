1f76e464b90d861ebc590763fa03fea4def82ceb
George McRedmond <georgem.int101@gmail.com>
2016-11-02 12:14:01 -0400

    Ships controller added

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index', as: 'homepage'
+
+
+ # Ship routes
+
+  get     "/ships"            	  =>     "ships#index", as: "ships"
+  get     "/ships/new"        	  =>     "ships#new", as: "new_ship"
+  post    "/ships"         	      =>     "ships#create"
+  get     "/ships/:id"            =>     "ships#show", as: "ship"
+  get     "/ships/:id/edit"       =>     "ships#edit", as: "edit_ship"
+  put     "/ships/:id"            =>     "ships#update"
+  patch   "/ships/:id"            =>     "ships#update"
+  delete  "/ships/:id"            =>     "ships#destroy"
+  
 end
