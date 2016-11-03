5757aae641a2bb721c2e1c8b807db1e11f1808b2
George McRedmond <georgem.int101@gmail.com>
2016-11-03 12:41:30 -0400

    marquee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def index
+  	@message = "You can see all of your users here..."
+  	@Users = User.all
+  end
+
+
   def show
 
     @message = "this is your profile page"
