b1e756ebe6002d72b61d7a6ac2b8b7d417ee15fb
jbedette <jbedette@yahoo.com>
2016-11-03 14:00:18 -0400

    generated devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102174947) do
+ActiveRecord::Schema.define(version: 20161103174015) do
+
+  create_table "contracts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "jobDescription"
