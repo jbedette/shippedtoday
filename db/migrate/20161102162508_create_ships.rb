073af3495565ec32e5f9763f00544824ce2f249c
jbedette <jbedette@yahoo.com>
2016-11-04 11:40:16 -0400

    nested routes for ships

--- db/migrate/20161102162508_create_ships.rb
+++ db/migrate/20161102162508_create_ships.rb
@@ -1,6 +1,7 @@
 class CreateShips < ActiveRecord::Migration[5.0]
   def change
     create_table :ships do |t|
+      # t.integer :user_id
       t.string :name
       t.integer :container
       t.string :location
