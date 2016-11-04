23d4951fe7e69de72f470b563c3a17d49b7b760a
George McRedmond <georgem.int101@gmail.com>
2016-11-04 15:45:35 -0400

    added paperclip

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,4 +4,11 @@ class Ship < ApplicationRecord
 	belongs_to :user
 	has_many :contracts
   has_many :jobs, through: :contracts
+
+  has_attached_file :avatar,
+ :styles => { :medium => "300x300>", :thumb => "200x200>" },
+ :default_url => "missing.png"
+validates_attachment_content_type :avatar,
+:content_type => /\Aimage\/.*\Z/
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103174015) do
+ActiveRecord::Schema.define(version: 20161104193227) do
 
   create_table "contracts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -36,8 +36,12 @@ ActiveRecord::Schema.define(version: 20161103174015) do
     t.string   "name"
     t.integer  "container"
     t.string   "location"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
